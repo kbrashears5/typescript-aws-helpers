e06ca48b331d4689184487de9d60f1f4b1576504
Kevin Brashears <kbrashears5@gmail.com>
2020-05-27 21:17:41 -0500

    complete refactor to use individual repo packages

--- src/handler.ts
+++ src/handler.ts
@@ -1,54 +1,41 @@
+
 import { Callback, Context, SQSRecord } from 'aws-lambda';
-import { APIGatewayHelper, BaseHelper, CloudWatchHelper, DynamoDBHelper, KMSHelper, LambdaHelper, S3Helper, SESHelper, SNSHelper, SQSHelper, SSMHelper } from './helpers';
-import { ILogger, Logger, LogLevel } from './logger';
-
-/**
- * IHandler
- */
-export interface IHandler {
-
-    APIGatewayHelper: APIGatewayHelper;
-    BaseHelper: BaseHelper;
-    CloudWatchHelper: CloudWatchHelper;
-    DynamoDBHelper: DynamoDBHelper;
-    KMSHelper: KMSHelper;
-    LambdaHelper: LambdaHelper;
-    Logger: ILogger;
-    S3Helper: S3Helper;
-    SESHelper: SESHelper;
-    SNSHelper: SNSHelper;
-    SQSHelper: SQSHelper;
-    SSMHelpler: SSMHelper;
-
-    Execute<T>(payload: T,
-               context: Context,
-               callback: Callback,
-               actionToExecute: () => Promise<T>): Promise<void>;
-
-    Orchestrate<T>(actionToExecute: () => Promise<T>): Promise<T>;
-
-    OrchestrateSQS(records: SQSRecord[],
-                   queueUrl: string,
-                   actionToExecute: () => Promise<void>): Promise<void>;
-}
+import { APIGatewayHelper, IAPIGatewayHelper } from 'typescript-aws-apigateway-helper';
+import { CloudWatchHelper, ICloudWatchHelper } from 'typescript-aws-cloudwatch-helper';
+import { DynamoHelper, IDynamoHelper } from 'typescript-aws-dynamo-helper';
+import { KMSHelper, IKMSHelper } from 'typescript-aws-kms-helper';
+import { LambdaHelper, ILambdaHelper } from 'typescript-aws-lambda-helper';
+import { S3Helper, IS3Helper } from 'typescript-aws-s3-helper';
+import { SESHelper, ISESHelper } from 'typescript-aws-ses-helper';
+import { SNSHelper, ISNSHelper } from 'typescript-aws-sns-helper';
+import { SQSHelper, ISQSHelper } from 'typescript-aws-sqs-helper';
+import { SSMHelper, ISSMHelper } from 'typescript-aws-ssm-helper';
+import { STSHelper, ISTSHelper } from 'typescript-aws-sts-helper';
+import { ILogger, LogLevel, Logger } from 'typescript-ilogger';
+import { IHandler } from './interface';
+import { LogHelper, ObjectOperations } from 'typescript-helper-functions';
 
 /**
  * Handles an incoming lambda execution request
  */
 export class Handler implements IHandler {
 
-    public APIGatewayHelper: APIGatewayHelper;
-    public BaseHelper: BaseHelper;
-    public CloudWatchHelper: CloudWatchHelper;
-    public DynamoDBHelper: DynamoDBHelper;
-    public KMSHelper: KMSHelper;
-    public LambdaHelper: LambdaHelper;
+    // AWS helpers
+    public APIGatewayHelper: IAPIGatewayHelper;
+    public CloudWatchHelper: ICloudWatchHelper;
+    public DynamoHelper: IDynamoHelper;
+    public KMSHelper: IKMSHelper;
+    public LambdaHelper: ILambdaHelper;
     public Logger: ILogger;
-    public S3Helper: S3Helper;
-    public SESHelper: SESHelper;
-    public SNSHelper: SNSHelper;
-    public SQSHelper: SQSHelper;
-    public SSMHelpler: SSMHelper;
+    public S3Helper: IS3Helper;
+    public SESHelper: ISESHelper;
+    public SNSHelper: ISNSHelper;
+    public SQSHelper: ISQSHelper;
+    public SSMHelper: ISSMHelper;
+    public STSHelper: ISTSHelper;
+
+    public LogHelper: LogHelper;
+    public ObjectOperations: ObjectOperations;
 
     /**
      * Initialize a new instance of Handler
@@ -61,16 +48,19 @@ export class Handler implements IHandler {
 
         // initialize all the helpers
         this.APIGatewayHelper = new APIGatewayHelper(this.Logger);
-        this.BaseHelper = new BaseHelper(this.Logger);
         this.CloudWatchHelper = new CloudWatchHelper(this.Logger);
-        this.DynamoDBHelper = new DynamoDBHelper(this.Logger);
+        this.DynamoHelper = new DynamoHelper(this.Logger);
         this.KMSHelper = new KMSHelper(this.Logger);
         this.LambdaHelper = new LambdaHelper(this.Logger);
         this.S3Helper = new S3Helper(this.Logger);
         this.SESHelper = new SESHelper(this.Logger);
         this.SNSHelper = new SNSHelper(this.Logger);
         this.SQSHelper = new SQSHelper(this.Logger);
-        this.SSMHelpler = new SSMHelper(this.Logger);
+        this.SSMHelper = new SSMHelper(this.Logger);
+        this.STSHelper = new STSHelper(this.Logger);
+
+        this.LogHelper = new LogHelper(this.Logger);
+        this.ObjectOperations = new ObjectOperations();
     }
 
     /**
@@ -81,14 +71,14 @@ export class Handler implements IHandler {
      * @param actionToExecute {function} Action to take
      */
     public async Execute<T>(payload: T,
-                            context: Context,
-                            callback: Callback<any>,
-                            actionToExecute: () => Promise<T>): Promise<void> {
+        context: Context,
+        callback: Callback<any>,
+        actionToExecute: () => Promise<T>): Promise<void> {
 
         const action = `${Handler.name}.${this.Execute.name}`;
-        this.BaseHelper.TraceInputs(action, { payload, context, callback });
+        this.LogHelper.LogInputs(action, { payload, context, callback });
 
-        if (this.BaseHelper.IsNullOrEmpty(context)) { throw new Error(`[${action}]-Must supply context`); }
+        if (this.ObjectOperations.IsNullOrEmpty(context)) { throw new Error(`[${action}]-Must supply context`); }
 
         let result: T | undefined;
         let error: Error | undefined;
@@ -129,12 +119,12 @@ export class Handler implements IHandler {
      * @param actionToExecute {function} Action to perform on each record
      */
     public async OrchestrateSQS(records: SQSRecord[],
-                                queueUrl: string,
-                                actionToExecute: () => Promise<void>): Promise<void> {
+        queueUrl: string,
+        actionToExecute: () => Promise<void>): Promise<void> {
 
         const action = `${Handler.name}.${this.OrchestrateSQS.name}`;
 
-        if (this.BaseHelper.IsNullOrEmpty(queueUrl)) { throw new Error(`[${action}]-Must supply queueUrl`); }
+        if (this.ObjectOperations.IsNullOrWhitespace(queueUrl)) { throw new Error(`[${action}]-Must supply queueUrl`); }
 
         await this.Orchestrate<void>(async () => {
             for (const record of records) {
---
+++ src/interface.ts
@@ -0,0 +1,43 @@
+import { Callback, Context, SQSRecord } from 'aws-lambda';
+import { IAPIGatewayHelper } from 'typescript-aws-apigateway-helper';
+import { ICloudWatchHelper } from 'typescript-aws-cloudwatch-helper';
+import { IDynamoHelper } from 'typescript-aws-dynamo-helper';
+import { IKMSHelper } from 'typescript-aws-kms-helper';
+import { ILambdaHelper } from 'typescript-aws-lambda-helper';
+import { IS3Helper } from 'typescript-aws-s3-helper';
+import { ISESHelper } from 'typescript-aws-ses-helper';
+import { ISNSHelper } from 'typescript-aws-sns-helper';
+import { ISQSHelper } from 'typescript-aws-sqs-helper';
+import { ISSMHelper } from 'typescript-aws-ssm-helper';
+import { ISTSHelper } from 'typescript-aws-sts-helper';
+import { ILogger } from 'typescript-ilogger';
+
+/**
+ * IHandler
+ */
+export interface IHandler {
+
+    APIGatewayHelper: IAPIGatewayHelper;
+    CloudWatchHelper: ICloudWatchHelper;
+    DynamoHelper: IDynamoHelper;
+    KMSHelper: IKMSHelper;
+    LambdaHelper: ILambdaHelper;
+    Logger: ILogger;
+    S3Helper: IS3Helper;
+    SESHelper: ISESHelper;
+    SNSHelper: ISNSHelper;
+    SQSHelper: ISQSHelper;
+    SSMHelper: ISSMHelper;
+    STSHelper: ISTSHelper;
+
+    Execute<T>(payload: T,
+        context: Context,
+        callback: Callback,
+        actionToExecute: () => Promise<T>): Promise<void>;
+
+    Orchestrate<T>(actionToExecute: () => Promise<T>): Promise<T>;
+
+    OrchestrateSQS(records: SQSRecord[],
+        queueUrl: string,
+        actionToExecute: () => Promise<void>): Promise<void>;
+}
